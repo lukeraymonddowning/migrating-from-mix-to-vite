71590c73a6a299685e664f2f8dca016446f4b0cd
Luke Downing <lukeraymonddowning@gmail.com>
2023-05-31 10:21:17 +0100

    Boilerplate

--- public/js/app.js
+++ public/js/app.js
@@ -19557,24 +19557,6 @@ window._ = (lodash__WEBPACK_IMPORTED_MODULE_0___default());
 window.axios = (axios__WEBPACK_IMPORTED_MODULE_1___default());
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-
 /***/ }),
 
 /***/ "./node_modules/lodash/lodash.js":
